193cd24023d4219295d159f3591957722fa86e39
unknown <novardewa@gmail.com>
2022-08-23 07:21:41 +0700

    first commit

--- app/Database/Migrations/2022-08-20-113417_GovernmentPlaceGallery.php
+++ app/Database/Migrations/2022-08-20-113417_GovernmentPlaceGallery.php
@@ -8,11 +8,36 @@ class GovernmentPlaceGallery extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 5,
+            ],
+            'government_place_id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 5,
+            ],
+            'url' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', TRUE);
+        $this->forge->addForeignKey('government_place_id', 'government_place', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('government_place_gallery');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('government_place_gallery');
     }
 }
--- app/Database/Migrations/2022-08-20-113434_GovernmentPlaceVideo.php
+++ app/Database/Migrations/2022-08-20-113434_GovernmentPlaceVideo.php
@@ -8,11 +8,40 @@ class GovernmentPlaceVideo extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 5,
+            ],
+            'government_place_id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 5,
+            ],
+            'url' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'duration' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', TRUE);
+        $this->forge->addForeignKey('government_place_id', 'government_place', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('government_place_video');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('government_place_video');
     }
 }
